37325b349db90b4e9dd6a95f6d4534895586db87
carlosid62@hotmail.com <carlosid62@hotmail.com>
2023-02-23 12:47:16 -0700

    init ramab

--- src/componentes/Project.js
+++ src/componentes/Project.js
@@ -1,18 +1,20 @@
 import React from "react";
-import ReactDOM from 'react-dom/client';
+import '../static/Project.css';
+
+
 
 class Project extends React.Component {
     constructor(props) {
         super(props);
     }
     render() {
-        return (                
-        <div class={this.props.title}>
-            <a href="#"><p>{this.props.title}</p></a>
-        </div>);
+        return (
+            <div className="project-title">
+                <button onClick={this.props.clickButton} href={this.props.source}>{this.props.title}</button>
+            </div>
+        );
     }
 }
 
-ReactDOM.render(<Project/>,document.getElementById("project"))
 
 export default Project
